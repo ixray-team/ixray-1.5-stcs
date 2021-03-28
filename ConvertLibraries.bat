8bf32ff59d4da813672ea78fbe94cec5a7a929a6
acidicMercury8 <acidicmercury8@outlook.com>
2021-03-28 15:01:07 +0300

    Set verbose converting COFF to OMF libraries

--- ConvertLibraries.bat
+++ ConvertLibraries.bat
@@ -9,9 +9,9 @@ if exist editor.lib (
     del editor.exp
 )
 
-coff2omf.exe BugTrap.lib BugTrapB.lib
-coff2omf.exe ETools.lib EToolsB.lib
-coff2omf.exe LWO.lib LWOB.lib
-coff2omf.exe DXT.lib DXTB.lib
+coff2omf.exe BugTrap.lib BugTrapB.lib -v
+coff2omf.exe ETools.lib EToolsB.lib -v
+coff2omf.exe LWO.lib LWOB.lib -v
+coff2omf.exe DXT.lib DXTB.lib -v
 
 pause
