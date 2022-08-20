5c8507dab4aa8dc5f9542e5a8d86fd10ba44072a
acidicMercury8 <acidicmercury8@outlook.com>
2022-08-20 21:50:41 +0300

    Replace broken STL extension aliases with modern analog

--- src/xrGame/game_cl_mp.h
+++ src/xrGame/game_cl_mp.h
@@ -52,7 +52,8 @@ struct cl_TeamStruct
 	float				Indicator_r2;
 };
 
-DEF_DEQUE(CL_TEAM_DATA_LIST, cl_TeamStruct);
+using CL_TEAM_DATA_LIST = xr_deque<cl_TeamStruct>;
+using CL_TEAM_DATA_LIST_it = CL_TEAM_DATA_LIST::iterator;
 
 struct cl_Message_Sound
 {
@@ -60,18 +61,25 @@ struct cl_Message_Sound
 	ref_sound	mSound_Radio;
 };
 
-DEF_VECTOR	(TEAMSOUND, cl_Message_Sound);
+using TEAMSOUND = xr_vector<cl_Message_Sound>;
+using TEAMSOUND_it = TEAMSOUND::iterator;
 
 struct cl_Menu_Message {
-	shared_str		pMessage;	
-	DEF_VECTOR	(SOUND_VARIANTS, TEAMSOUND);
+	shared_str		pMessage;
+
+	using SOUND_VARIANTS = xr_vector<TEAMSOUND>;
+	using SOUND_VARIANTS_it = SOUND_VARIANTS::iterator;
+
 	SOUND_VARIANTS		aVariants;
 };
 
 struct cl_MessageMenu
 {
 	CUISpeechMenu*		m_pSpeechMenu;
-	DEF_VECTOR	(MENUMESSAGES, cl_Menu_Message);
+
+	using MENUMESSAGES = xr_vector<cl_Menu_Message>;
+	using MENUMESSAGES_it = MENUMESSAGES::iterator;
+
 	MENUMESSAGES		m_aMessages;	
 
 	bool operator == (CUISpeechMenu* pMenu){return pMenu == m_pSpeechMenu;}
@@ -117,13 +125,20 @@ protected:
 
 	CL_TEAM_DATA_LIST				TeamList;
 
-	DEF_VECTOR(SNDMESSAGES, SND_Message);
+	using SNDMESSAGES = xr_vector<SND_Message>;
+	using SNDMESSAGES_it = SNDMESSAGES::iterator;
+
 	SNDMESSAGES						m_pSndMessages;
 	bool							m_bJustRestarted;
-	DEF_VECTOR(SNDMESSAGESINPLAY, SND_Message*);
+
+	using SNDMESSAGESINPLAY = xr_vector<SND_Message*>;
+	using SNDMESSAGESINPLAY_it = SNDMESSAGESINPLAY::iterator;
+
 	SNDMESSAGESINPLAY				m_pSndMessagesInPlay;
 
-	DEF_VECTOR(BONUSES, Bonus_Struct);
+	using BONUSES = xr_vector<Bonus_Struct>;
+	using BONUSES_it = BONUSES::iterator;
+
 	BONUSES							m_pBonusList;
 
 	bool							m_bVotingActive;
