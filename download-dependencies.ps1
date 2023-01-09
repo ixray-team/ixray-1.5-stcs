ed64e58f086e7e1bbf82d8b5882626467027439c
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-09 21:30:43 +0300

    Add DirectXMesh

--- download-dependencies.ps1
+++ download-dependencies.ps1
@@ -16,6 +16,7 @@ If (!(Test-Path "sdk\dxsdk_mar2009")) {
 
 # Getting another dependencies from Git
 git clone --branch jul2022 --depth 1 https://github.com/microsoft/DirectXTex.git dep/DirectXTex
+git clone --branch jul2022 --depth 1 https://github.com/microsoft/DirectXMesh.git dep/DirectXMesh
 git clone --branch Release_3.1.0 --depth 1 https://github.com/dockpanelsuite/dockpanelsuite.git dep/dockpanelsuite
 
 # Pause
