ee875a4532878d4096a12a78ef7304df62ba30bb
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-12 22:46:26 +0300

    Move generate patch commands to script

---
+++ generate-patch.bat
@@ -0,0 +1,14 @@
+:: Get assets
+git clone ./.git ./temp
+cd temp
+git checkout f23bc66ed4a4a6e75d8c9000bf8731d3adc062d7
+cd ..
+move temp/gamedata gamedata_origin
+
+:: Generate patch
+bin\xrCompress.exe -diff gamedata gamedata_origin -out patch
+
+:: Pack patch
+bin\xrCompress.exe patch -ltx datapack.ltx
+mkdir patches
+move patch.pack_#0 patches\xpatch_11.db
