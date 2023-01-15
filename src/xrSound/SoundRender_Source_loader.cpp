358ad127b4a645930bd600c6b7dab6d78fbd572f
Drombeys <dima.zolotov02@gmail.com>
2023-01-15 21:07:42 +0300

    Replace crash with warning when there is no sound

--- src/xrSound/SoundRender_Source_loader.cpp
+++ src/xrSound/SoundRender_Source_loader.cpp
@@ -138,11 +138,11 @@ void CSoundRender_Source::load(LPCSTR name)
 	strconcat			(sizeof(fn),fn,N,".ogg");
 	if (!FS.exist("$level$",fn))	FS.update_path	(fn,"$game_sounds$",fn);
 
-#ifdef _EDITOR
-	if (!FS.exist(fn)){ 
+	if (!FS.exist(fn)) { 
 		FS.update_path	(fn,"$game_sounds$","$no_sound.ogg");
+		Msg("! Can't find sound '%s'", fname.c_str());
     }
-#endif
+
 	LoadWave			(fn);
 	SoundRender->cache.cat_create	(CAT, dwBytesTotal);
 }
