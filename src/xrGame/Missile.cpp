091f90f623a66ca7bc84f2bda7397580b774ff56
Shtrecker <77389805+Shtrecker@users.noreply.github.com>
2023-04-02 14:36:22 +0300

    Fix animation playing of item throwing

--- src/xrGame/Missile.cpp
+++ src/xrGame/Missile.cpp
@@ -545,9 +545,11 @@ bool CMissile::Action(s32 cmd, u32 flags)
 			m_constpower = true;			
 			if(flags&CMD_START) 
 			{
-				m_throw = true;
 				if(GetState()==eIdle) 
+				{
+					m_throw = true;
 					SwitchState(eThrowStart);
+				}
 			} 
 			return true;
 		}break;
