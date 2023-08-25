54ab11a19beca16ab13dc8fb23e40c2cc873059c
Drombeys <dima.zolotov02@gmail.com>
2023-08-25 14:06:22 +0300

    Delete `bug` option in all renders

--- src/Layers/xrRenderPC_R2/r2_rendertarget_phase_combine.cpp
+++ src/Layers/xrRenderPC_R2/r2_rendertarget_phase_combine.cpp
@@ -74,13 +74,12 @@ void	CRenderTarget::phase_combine	()
 	}
 
 	// 
-	//if (RImplementation.o.bug)	{
-		RCache.set_Stencil					(TRUE,D3DCMP_LESSEQUAL,0x01,0xff,0x00);	// stencil should be >= 1
-		if (RImplementation.o.nvstencil)	{
-			u_stencil_optimize				(FALSE);
-			RCache.set_ColorWriteEnable		();
-		}
-	//}
+
+	RCache.set_Stencil(TRUE, D3DCMP_LESSEQUAL, 0x01, 0xff, 0x00);	// stencil should be >= 1
+	if (RImplementation.o.nvstencil) {
+		u_stencil_optimize(FALSE);
+		RCache.set_ColorWriteEnable();
+	}
 
 	// calc m-blur matrices
 	Fmatrix		m_previous, m_current;
