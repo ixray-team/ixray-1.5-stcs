9e082e5a74d89065da574df2bf3f086bc5c04576
Hrusteckiy <kostia4554t@yandex.ru>
2023-09-06 16:20:20 +0300

    Implement custom text feature
    Add in section of an item:
    
    ```
    item_custom_text = st_text
    item_custom_text_clr_inv = 100,250,100
    item_custom_text_font = graffiti22
    item_custom_text_offset = -2.0, -2.0
    ```
    
    item_custom_text_clr_inv, item_custom_text_font, item_custom_text_offset
    are an optional, if its not written will be used font, color and offset from
    XML node. item_custom_text is a text from StringTable.
    item_custom_text_offset is an offset by right-bottom of cell

--- src/xrGame/ui/UIXmlInit.cpp
+++ src/xrGame/ui/UIXmlInit.cpp
@@ -4,6 +4,7 @@
 #include "../UI.h"
 #include "../level.h"
 #include "../UICustomItem.h"
+#include "../UIFontDefines.h"
 
 #include "../string_table.h"
 #include "UIFrameWindow.h"
@@ -34,21 +35,6 @@
 
 extern int keyname_to_dik(LPCSTR);
 
-#define ARIAL_FONT_NAME			"arial"
-
-#define MEDIUM_FONT_NAME		"medium"
-#define SMALL_FONT_NAME			"small"
-
-#define GRAFFITI19_FONT_NAME	"graffiti19"
-#define GRAFFITI22_FONT_NAME	"graffiti22"
-#define GRAFFITI32_FONT_NAME	"graffiti32"
-#define GRAFFITI50_FONT_NAME	"graffiti50"
-
-#define LETTERICA16_FONT_NAME	"letterica16"
-#define LETTERICA18_FONT_NAME	"letterica18"
-#define LETTERICA25_FONT_NAME	"letterica25"
-
-#define DI_FONT_NAME			"di"
 
 //////////////////////////////////////////////////////////////////////////
 
@@ -784,7 +770,7 @@ bool CUIXmlInit::InitFont(CUIXml &xml_doc, LPCSTR path, int index, u32 &color, C
 		{
 			pFnt = UI().Font().pFontGraffiti50Russian;
 		}
-		else if(!xr_strcmp(*font_name, "arial_14"))
+		else if(!xr_strcmp(*font_name, ARIAL14_FONT_NAME))
 		{
 			pFnt = UI().Font().pFontArial14;
 		}
