a800bca6d8812b0465bb812a05ff6f6833d9b08d
Drombeys <dima.zolotov02@gmail.com>
2023-10-15 10:45:54 +0300

    Move `CFontManager` struct in `ui_base.h`

--- src/xrGame/ui_base.h
+++ src/xrGame/ui_base.h
@@ -3,7 +3,6 @@
 #define UI_BASE_WIDTH	1024.0f
 #define UI_BASE_HEIGHT	768.0f
 
-struct CFontManager;
 class CUICursor;
 
 #include "../Include/xrRender/UIRender.h"
@@ -20,7 +19,35 @@ public:
 
 };
 
+struct CFontManager :public pureDeviceReset {
+	CFontManager();
+	~CFontManager();
 
+	typedef xr_vector<CGameFont**>					FONTS_VEC;
+	typedef FONTS_VEC::iterator						FONTS_VEC_IT;
+	FONTS_VEC				m_all_fonts;
+	void					Render();
+
+	// hud font
+	CGameFont* pFontMedium;
+	CGameFont* pFontDI;
+
+	CGameFont* pFontArial14;
+	CGameFont* pFontGraffiti19Russian;
+	CGameFont* pFontGraffiti22Russian;
+	CGameFont* pFontLetterica16Russian;
+	CGameFont* pFontLetterica18Russian;
+	CGameFont* pFontGraffiti32Russian;
+	CGameFont* pFontGraffiti50Russian;
+	CGameFont* pFontLetterica25;
+	CGameFont* pFontStat;
+
+	void					InitializeFonts();
+	void					InitializeFont(CGameFont*& F, LPCSTR section, u32 flags = 0);
+	LPCSTR					GetFontTexName(LPCSTR section);
+
+	virtual void			OnDeviceReset();
+};
 
 //---------------------------------------------------------------------------------------
 // 2D Frustum & 2D Vertex
