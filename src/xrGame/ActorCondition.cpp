e9c14b918a5883de61e63570c62aa2099efb4d5f
Drombeys <dima.zolotov02@gmail.com>
2023-11-11 20:30:02 +0300

    Delete unused files in xrGame

--- src/xrGame/ActorCondition.cpp
+++ src/xrGame/ActorCondition.cpp
@@ -12,7 +12,6 @@
 #include "script_callback_ex.h"
 #include "script_game_object.h"
 #include "game_object_space.h"
-#include "ui\UIVideoPlayerWnd.h"
 #include "script_callback_ex.h"
 #include "object_broker.h"
 #include "weapon.h"
