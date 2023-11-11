6c4aaf49d3844f425646bc7fc36972a7f67eb672
Drombeys <dima.zolotov02@gmail.com>
2023-11-11 14:27:19 +0300

    Delete blackbox dep

--- src/xrCore/xrDebug.h
+++ src/xrCore/xrDebug.h
@@ -58,6 +58,8 @@ IC	std::string __cdecl	make_string		(LPCSTR format,...)
 
 extern XRCORE_API	xrDebug		Debug;
 
+XRCORE_API void LogStackTrace	(LPCSTR header);
+
 #include "xrDebug_macros.h"
 
 #endif // xrDebugH
